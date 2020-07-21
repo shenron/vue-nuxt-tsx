6e5adbcc27e60ff7462bcfc2d76cc1022c6f2d8d
Thomas CASTELLY <thomas@tcy.io>
2020-07-21 18:05:50 +0200

    remove debugger

--- src/plugins/provideApolloClient.ts
+++ src/plugins/provideApolloClient.ts
@@ -2,7 +2,6 @@ import { provide, onGlobalSetup } from 'nuxt-composition-api';
 import { DefaultApolloClient } from '@vue/apollo-composable';
 
 export default ({ app }) => {
-  debugger;
   onGlobalSetup(() => {
     const apolloClient = app.apolloProvider.defaultClient;
 
